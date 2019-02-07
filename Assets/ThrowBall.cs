227a22546c1c2d691aa6c8793e11beb85be955ab
Cody Regalado <rega5312@csumb.ad.csumb.edu>
2019-02-07 12:42:51 -0800

    adding player code

---
+++ Assets/ThrowBall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowBall : MonoBehaviour {
+
+    public GameObject ballObj;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = 2.0f;
+            Vector3 ballPos = Camera.current.ScreenToWorldPoint(mousePos);
+            Instantiate(ballObj, ballPos, Quaternion.identity);
+        }
+	}
+}
