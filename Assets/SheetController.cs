9a5cf7afba66d162326192d427cf86aee5178558
Eric <gonzerc526@gmail.com>
2019-02-11 21:40:00 -0800

    AddTorque on controller sheet

---
+++ Assets/SheetController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheetController : MonoBehaviour {
+    public float power;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        rb.AddTorque(transform.up * power * h);
+    }
+}
